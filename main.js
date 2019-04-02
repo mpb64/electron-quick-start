f59ad8cf266fbe98b1c03e7d84c8a8d59a501e4a
mpb64 <mwilh003@gmail.com>
2019-04-02 11:07:32 -0400

    Added code to show bug where getProcessMemoryInfo is missing from the process api in sandbox mode

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -11,12 +12,17 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+	  nodeIntegration: false,
+	  sandbox: true,
+	  preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  mainWindow.webContents.once('dom-ready', () => {
+	mainWindow.webContents.send('memory-info');
+})
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
--- renderer.js
+++ renderer.js
@@ -1,3 +1,25 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// All of the Node.js APIs are available in this process
+
+
+window.ipcRenderer.on('memory-info', callGetProcessMemoryInfo);
+
+async function callGetProcessMemoryInfo() {
+	let process_func_list = "";
+	for (var key in window.process) {
+		if (typeof window.process[key] === 'function') {
+			process_func_list = process_func_list += `${key}, `;
+		}
+	}
+	document.getElementById('process-functions').appendChild(document.createTextNode(process_func_list))
+	let memoryUsage;
+	try {
+		memoryUsage = await window.process.getProcessMemoryInfo();
+		console.log('memory usage', memoryUsage);
+	} catch(e) {
+		console.log('error', e);
+		document.getElementById('error').appendChild(document.createTextNode(e))
+	}
+
+}
