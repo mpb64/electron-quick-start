72ba156d943b8d7d86b64110a3585f8c530e9c94
mpb64 <mwilh003@gmail.com>
2019-04-02 13:43:44 -0400

    Changed tabs to spaces

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-	  nodeIntegration: false,
-	  sandbox: true,
-	  preload: path.join(__dirname, 'preload.js')
+    nodeIntegration: false,
+      sandbox: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
--- renderer.js
+++ renderer.js
@@ -6,20 +6,20 @@
 window.ipcRenderer.on('memory-info', callGetProcessMemoryInfo);
 
 async function callGetProcessMemoryInfo() {
-	let process_func_list = "";
-	for (var key in window.process) {
-		if (typeof window.process[key] === 'function') {
-			process_func_list = process_func_list += `${key}, `;
-		}
-	}
-	document.getElementById('process-functions').appendChild(document.createTextNode(process_func_list))
-	let memoryUsage;
-	try {
-		memoryUsage = await window.process.getProcessMemoryInfo();
-		console.log('memory usage', memoryUsage);
-	} catch(e) {
-		console.log('error', e);
-		document.getElementById('error').appendChild(document.createTextNode(e))
-	}
+    let process_func_list = "";
+    for (var key in window.process) {
+        if (typeof window.process[key] === 'function') {
+            process_func_list = process_func_list += `${key}, `;
+        }
+    }
+    document.getElementById('process-functions').appendChild(document.createTextNode(process_func_list))
+    let memoryUsage;
+    try {
+        memoryUsage = await window.process.getProcessMemoryInfo();
+        console.log('memory usage', memoryUsage);
+    } catch(e) {
+        console.log('error', e);
+        document.getElementById('error').appendChild(document.createTextNode(e))
+    }
 
 }
